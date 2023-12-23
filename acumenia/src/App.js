226dad77260e5387153a04ddc9cdaf739dbdb4dd
Charitha2009 <70010999+Charitha2009@users.noreply.github.com>
2023-12-23 10:44:01 -0500

    Added OurStory page

--- acumenia/src/App.js
+++ acumenia/src/App.js
@@ -1,17 +1,24 @@
 // src/App.js
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faGlobe, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faGlobe, faUser, faHome } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link, Outlet, useNavigate } from 'react-router-dom';
+import OurStory from './OurStory';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const redirectToOurStory = () => {
+    navigate('/ourStory', { replace: true });
+  };
   return (
     <header>
       
       <nav>
         <ul>
-         
-          <li><FontAwesomeIcon icon={faGlobe} style={{color: "#000000", marginRight:'7px'}} />
+          <li><FontAwesomeIcon icon={faHome} style={{ color: '#000000', marginRight:'7px'}} />Home</li>
+          <li onClick={redirectToOurStory}><FontAwesomeIcon icon={faGlobe} style={{color: "#000000", marginRight:'7px'}} />
       Our Story</li>
           <li><FontAwesomeIcon icon={faPhone} style={{ color: '#000000', marginRight:'7px'}} />Contact</li>
           <li><FontAwesomeIcon icon={faUser} style={{color: "#000000", marginRight:'7px'}} />Login</li>
@@ -25,7 +32,7 @@ const MainContent = () => {
   return (
     <div className="container" >
       <div className="text" style={{ marginBottom: '1000px', display:'flex',flexDirection:'column' }}>
-        <p style={{marginTop:'100px',padding:'0', fontSize:'80px',fontWeight:'bolder'}}><span style={{color:'#C7372F'}}>A</span>cumenia <br></br><span style={{fontSize:'20px',marginLeft:'140px',marginTop:'-80px'}}>~learn grow and discover</span></p>
+        <p style={{marginTop:'100px',padding:'0', fontSize:'80px',fontWeight:'bolder'}}><span style={{color:'#C7372F'}}>A</span>cumenia <br></br><span style={{fontSize:'20px',marginLeft:'140px',marginTop:'-80px'}}>~learn, discover and grow</span></p>
          
         <button style={{ width:'250px',fontSize: '20px', padding: '25px', backgroundColor: 'black', color: 'white', borderRadius: '40px', cursor: 'pointer', marginLeft:'55px' ,marginTop:'-50px' }}>
     Get Started
@@ -49,11 +56,18 @@ const Footer = () => {
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <Header />
       <MainContent />
       <Footer />
     </div>
+    <Routes>
+      <Route path="/ourStory" element={<OurStory />} />
+      </Routes>
+    </Router>
+
   );
 }
 
---
+++ acumenia/src/OurStory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OurStory = () => {
+  return (<div>
+    <h2>Our Story Page</h2>
+    <h2>Our Story Page</h2>
+    <h2>Our Story Page</h2>
+
+    </div>);
+};
+
+export default OurStory;
